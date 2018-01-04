a8111941ba5b5f1f993a26222ef85dd8c71b6eb9
tim.allen@bjss.com <tim.allen@bjss.com>
2018-01-04 15:41:48 +0000

    A simple Azure Function to send an email via SendGrid using Azure Functions

---
+++ EmailFunctions/EmailMessage.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace EmailFunctions
+{
+    public class EmailMessage
+    {
+        [JsonProperty(PropertyName = "fromAddress")]
+        public string FromEmailAddress { get; set; }
+
+        [JsonProperty(PropertyName = "fromName")]
+        public string FromName { get; set; }
+
+        [JsonProperty(PropertyName = "subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty(PropertyName = "body")]
+        public string Body { get; set; }
+
+        [JsonProperty(PropertyName = "isHtml")]
+        public bool IsHtml { get; set; }
+
+        [JsonProperty(PropertyName = "toAddresses") ]
+        public List<string> ToEmailAddresses { get; } = new List<string>();
+    }
+}
---
+++ EmailFunctions/SendEmail.cs
@@ -0,0 +1,59 @@
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System.Collections.Generic;
+
+namespace EmailFunctions
+{
+    public static class SendEmail
+    {
+        [FunctionName("SendEmail")]
+        public static async Task<object> Run([HttpTrigger(WebHookType = "genericJson")]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"Webhook was triggered!");
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            var message = JsonConvert.DeserializeObject<EmailMessage>(jsonContent);
+
+            var apiKey = System.Environment.GetEnvironmentVariable("ApiKey", System.EnvironmentVariableTarget.Process);
+            var client = new SendGridClient(apiKey /*"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"*/);
+            var msg = BuildEmailMessage(message);
+
+            var response = client.SendEmailAsync(msg).Result;
+
+            if (response.StatusCode == HttpStatusCode.Accepted)
+            {
+                return req.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        private static SendGridMessage BuildEmailMessage(EmailMessage message)
+        {
+            var mimeType = message.IsHtml ? MimeType.Html : MimeType.Text;
+            var receipients = new List<EmailAddress>();
+
+            foreach (var toAddress in message.ToEmailAddresses)
+            {
+                receipients.Add(new EmailAddress(toAddress));
+            }
+
+            var msg = new SendGridMessage();
+
+            msg.SetFrom(new EmailAddress(message.FromEmailAddress, message.FromName));
+            msg.AddTos(receipients);
+            msg.SetSubject(message.Subject);
+            msg.AddContent(mimeType, message.Body);
+
+            return msg;
+        }
+    }
+}
